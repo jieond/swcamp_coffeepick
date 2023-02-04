6d9706e40952e92c457b5ef4c05f7afdaff72270
sykim <sykimworker@gmail.com>
2023-02-04 18:53:49 +0900

    2/4 최종 수정본

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def web_order_get():
     return jsonify({'orders': order_list})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5004, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
